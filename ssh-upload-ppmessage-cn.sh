b2431686e21363f49783d5e31fa0089f7bfe78f5
Tiecheng Sun <suntiecheng@foxmail.com>
2018-04-13 13:31:45 +0800

    add pull

--- ssh-upload-ppmessage-cn.sh
+++ ssh-upload-ppmessage-cn.sh
@@ -1,5 +1,5 @@
 
-git add .;git commit -m "publish"; git push origin master;
+git pull; git add .;git commit -m "publish"; git push origin master;
 ssh root@ppmessage.cn 'bash -s' < copy-book-to-nginx.sh
 
 
